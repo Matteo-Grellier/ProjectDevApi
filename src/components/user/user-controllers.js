fb5191b5cac4ca7f6c5753635067e4a4661d05d4
matteo-grellier <matteogrellier@gmail.com>
2022-12-12 11:08:44 +0100

    :sparkles: add login feature

--- src/components/user/user-controllers.js
+++ src/components/user/user-controllers.js
@@ -4,7 +4,7 @@ import argon2, { hash } from 'argon2'
 import { sendWelcomeEmail } from '#services/mailing/welcome-email.js'
 
 export async function register (ctx) {
- try {
+  try {
   const registerValidationSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
@@ -26,19 +26,72 @@ export async function register (ctx) {
   await sendWelcomeEmail(user, user.settings.validation_email_token)
   const token = user.generateJWT()
   ctx.ok({ token })
- } catch(e) {
-  ctx.badRequest({ message: e.message })
- }
+  } catch(e) {
+    ctx.badRequest({ message: e.message })
+  }
 }
 
-export function profile (ctx) {
- try {
-  ctx.ok(ctx.state.user)
- } catch(e) {
-  ctx.badRequest({ message: e.message })
- }
-}
-
+// login
 export async function login (ctx) {
+  try {
+    const loginValidationSchema = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required()
+    })
+    const params = ctx.request.body
+    const { error, value } = loginValidationSchema.validate(params)
+    if(error) throw new Error(error)
+    const user = await UserModel
+      .findOne
+      ({ email: value.email })
+      .select('+password')
+    if(!user) throw new Error('User not found')
+    const isPasswordValid = await argon2.verify(user.password, value.password)
+    if(!isPasswordValid) throw new Error('Invalid password')
+    const token = user.generateJWT()
+    ctx.ok({ token })
+  } catch(e) {
+    ctx.badRequest({ message: e.message })
+  }
+}
 
+// get profile of the current logged in user
+export async function profile (ctx) {
+  try {
+    const user
+    = await UserModel
+      .findById(ctx.state.user.id)
+      .select('-password')
+    if(!user) throw new Error('User not found')
+    ctx.ok({ user })
+  } catch(e) {
+    ctx.badRequest({ message: e.message })
+  }
+}
+
+// modify user
+export async function modify (ctx) {
+  try {
+    const modifyValidationSchema = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+      terms_and_conditions: Joi.boolean().valid(true).required()
+    })
+    const params = ctx.request.body
+    const { error, value } = modifyValidationSchema.validate(params)
+    if(error) throw new Error(error)
+    const hashedPassword = await argon2.hash(value.password)
+    const user = await UserModel
+      .findByIdAndUpdate(ctx.state.user.id, {
+        ...value,
+        password: hashedPassword,
+        settings: {
+          terms_and_conditions: value.terms_and_conditions
+        }
+      })
+    if(!user) throw new Error('User not found')
+    ctx.ok({ user })
+  } catch(e) {
+    ctx.badRequest({ message: e.message })
+  }
 }
--- src/components/user/user-routes.js
+++ src/components/user/user-routes.js
@@ -1,10 +1,15 @@
 import Router from '@koa/router'
 import * as UserControllers from '#components/user/user-controllers.js'
+import { isAuthenticatedWithUser } from '#middlewares/jwt-handler.js'
 
 const users =  new Router()
 
 users.post('/register', UserControllers.register)
 users.post('/login', UserControllers.login)
-users.post('/profile', UserControllers.profile)
+
+users.get('/profile', isAuthenticatedWithUser, (ctx) => {
+  ctx.ok({ user: ctx.state.user })
+})
+users.post('/modify', isAuthenticatedWithUser, UserControllers.modify)
 
 export default users
