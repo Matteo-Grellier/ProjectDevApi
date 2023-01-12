522b3551937a1e9699417a4559eff7b7a2c4941f
matteo-grellier <matteogrellier@gmail.com>
2023-01-12 18:17:33 +0100

    :lipstick: Rework the navbar

--- front/src/layouts/MainLayout.vue
+++ front/src/layouts/MainLayout.vue
@@ -2,17 +2,21 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated class="bg-white text-black">
       <q-toolbar class="row">
-        <q-btn class="col-1" to="dashboard">
+        <q-btn to="dashboard">
           <img
           alt="logo"
           src="~assets/logo.png"
           style="width: 30px; height: 30px"
           >
         </q-btn>
-        <router-link float-right to="/login" class="col-1 justify-end">
+        <q-btn float-right outline label="Lists" color="primary" no-caps class="q-mx-md col-1 flex" to="/lists" />
+        <div class="loginContainer">
+        <router-link float-right to="/login" class="col-1 justify-end login flex">
           Se connecter
         </router-link>
-        <q-btn float-right outline label="S'inscrire" color="primary" no-caps class="q-mx-md col-1" to="/register" />
+
+        <q-btn float-right outline label="S'inscrire" color="primary" no-caps class="q-mx-md col-1 register flex" to="/register" />
+        </div>
       </q-toolbar>
     </q-header>
     <q-page-container>
@@ -21,5 +25,16 @@
   </q-layout>
 </template>
 
-<script>
-</script>
+<style lang="css">
+
+.loginContainer{
+  justify-content: flex-end;
+  display: flex;
+  width: 100%;
+}
+
+.login{
+  align-items: center;
+}
+
+</style>
